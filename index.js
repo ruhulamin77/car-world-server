5b859f256707a38e1780ddcc878688ebba77ba04
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-11-13 19:42:53 +0600

    manage product done

--- index.js
+++ index.js
@@ -138,6 +138,15 @@ async function run() {
       const result = await ordersCollection.deleteOne(query);
       res.json(result);
     });
+
+    // delete product
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carsCollection.deleteOne(query);
+      console.log("hit delete", result);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
